5481287f5a8ef2e5288b1f4754ecc77464767d32
Hochwald <ehochwal@ezesoft.com>
2023-02-27 15:21:07 -0500

    created the program class which prompts user for employee info, starting on the Util class to create csv file

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker {
+    class Employee {
+        // by default properties defined in a class are private, meaning that can only be accessed within the class
+        // must give it the public access modifier
+        private string FirstName;
+        private string LastName;
+        private int Id;
+        private string PhotoUrl;
+        public Employee(string firstName, string lastName, int id, string photoUrl) {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            PhotoUrl = photoUrl;
+        }
+        public string GetFullName() {
+            return FirstName + " " + LastName;
+        }
+        public int GetId() {
+            return Id;
+        }
+        public string GetPhotoUrl() {
+            return PhotoUrl;
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            List<Employee> employees = GetEmployees();
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
+
+
+            static List<Employee> GetEmployees() {
+                List<Employee> employees = new List<Employee>();
+                // Collect user values until the value is an empty string
+                while (true) {
+                    Console.WriteLine("Enter first name (leave empty to exit): ");
+                    // Get a name from the console and assign it to a variable
+                    // uses the null coalescing operator ??
+                    // this will check for null and replace the null value with the value after the operator
+                    string firstName = Console.ReadLine() ?? "";
+                    if (firstName == "") break;
+                    Console.Write("Enter last name: ");
+                    string lastName = Console.ReadLine() ?? "";
+                    Console.Write("Enter ID: ");
+                    // ReadLine() only returns string types, so need to cast to int
+                    int id = Int32.Parse(Console.ReadLine() ?? "");
+                    Console.Write("Enter Photo URL:");
+                    string photoUrl = Console.ReadLine() ?? "";
+                    Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                    employees.Add(currentEmployee);
+                }
+                return employees;
+            }
+        }
+    }
+}
